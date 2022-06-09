921088ed42250a94ba49f8697cd91e6377a61cee
sizahncube <106956592+sizahncube@users.noreply.github.com>
2022-06-09 22:30:01 +0200

    Update settings.py

--- solusi/settings.py
+++ solusi/settings.py
@@ -147,9 +147,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'solusiweb/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 
 
 
